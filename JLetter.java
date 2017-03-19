155095c7775b76db6c4a2e51ab138e42974b9479
Amber J <asj0017@auburn.edu>
2017-03-19 00:13:46 -0500

    Update JLetter.java

--- JLetter.java
+++ JLetter.java
@@ -1,17 +1,6 @@
-/**
- * Prints the letter J using the letters J, A, and V.
- *
- * @author Amber Jackson
- * @version 01-21-2016
- */
 
 public class JLetter {
-    /**
-     * Prints to std output.
-     *
-     * @param args Command line arguments (not used).
-     */
-
+    
    public static void main(String[] args) {
       System.out.println("JAVAJAVAJAVA\n"
          + "JAVAJAVAJAVA\n"
